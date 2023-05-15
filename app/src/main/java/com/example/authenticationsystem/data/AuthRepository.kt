a99f0fc98c8f4ed834214c394561116ab3732e4f
Raspac <75386339+Raspac@users.noreply.github.com>
2023-05-15 19:04:11 +0200

    .

--- app/src/main/java/com/example/authenticationsystem/data/AuthRepository.kt
+++ app/src/main/java/com/example/authenticationsystem/data/AuthRepository.kt
@@ -2,7 +2,8 @@ package com.example.authenticationsystem.data
 
 import kotlinx.coroutines.flow.Flow
 import com.example.authenticationsystem.util.Resource
+import com.google.firebase.AuthResult
 
 interface AuthRepository {
-    fun loginUser(email:String, password:String): Flow<>
+    fun loginUser(email:String, password:String): Flow<Resource>
 }
